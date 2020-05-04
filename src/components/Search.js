5a9f6a23e1cda092fd58c04feceb810307c23987
Yeng Vang <yenghuevang@hotmail.com>
2020-05-03 19:53:36 -0500

    added files

---
+++ src/components/Search.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+
+function Search(props) {
+  return (
+    
+    <div className="searchBar">
+    <input type="search" id="search" placeholder="Search Employee" />
+    </div>
+  );
+}
+
+export default Search;
